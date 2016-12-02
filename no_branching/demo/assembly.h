ba202ffb0392133d2582ec60c3ba517a696c2c8c
Trent Andraka <stealthbomber10@gmail.com>
2016-12-02 05:48:28 -0600

    Add files via upload
    Moving files from school-associated Bitbucket account to personal Github for more ease of use / accesibility

---
+++ no_branching/demo/assembly.h
@@ -0,0 +1,8 @@
+// Function prototypes for assembly file.
+extern "C" {
+  boolean hasAOne(byte num);
+  char intToAscii(byte num);
+  int byteToInt(byte num);
+  int addBytes(byte num1, byte num2);
+  char average(byte num1, byte num2);
+}
